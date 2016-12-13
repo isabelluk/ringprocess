4a228dfa0737f68fd76d1eb47a45572ea75b5e11
Isabel Luk <ipyluk@kth.se>
2016-12-13 21:52:09 +0100

    add round counter

--- process.erl
+++ process.erl
@@ -1,31 +1,36 @@
 -module(process).
--export([start/1,create_node/1,create_node/2,loop/1]).
+-export([start/2,create_node/2,create_node/3,loop/2]).
 
-
-start(N) ->
+start(N,R) ->
     io:format("Spawning ~w nodes!~n",[N]),
-    spawn(process,create_node,[N]). 
+    spawn(process,create_node,[N,R]). 
 
-create_node(N) ->
+create_node(N,R) ->
     io:format("Creating node ~w (~w)~n", [N,self()]),
-    create_node(N,self()).
+    create_node(N,self(),R).
 
-create_node(1,NextPid) ->
+create_node(1,NextPid,R) ->
 	NextPid ! token,
-    loop(NextPid);
-create_node(N,NextPid) ->
-    PrevPid = spawn_link(process, loop, [NextPid]),
-    create_node(N - 1, PrevPid).
+    register(head, self()),
+    loop(NextPid,R);
+create_node(N,NextPid,R) ->
+    PrevPid = spawn_link(process, loop, [NextPid,R]),
+    create_node(N - 1, PrevPid,R).
 
-% stop() ->
-%     head ! stop,
-%     ok. 
-
-loop(NextPid) ->
-    receive
-        token ->
-        	io:fwrite("passing it to ~p~n",[NextPid]),
+loop(NextPid,R) ->
+    case R of
+        R when R > 0 ->
             NextPid ! token,
-            loop(NextPid),
-            ok
+            receive
+                token ->
+        	       io:fwrite("(~w) passing it to ~p~n",[R,NextPid]),
+                   loop(NextPid,R-1)
+            end;
+        0 ->
+            NextPid ! stop,
+            receive
+                stop ->
+                    NextPid ! stop,
+                    ok
+            end
     end.
